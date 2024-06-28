c29a24a54af66054a9e98ba43f7cac7772be269e
13jalda <j.aldacarro@gmail.com>
2024-06-28 11:11:44 +0200

    Auth error

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
-import moment from 'moment';
+
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route 
+} from "react-router-dom";
+
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import NoMatch from './pages/no-match';
+import PortfolioDetail from './portfolio/portfolio-detail';
+import Auth from './pages/auth';
+
+
 
 export default class App extends Component {
+    
   render() {
+    
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-        <h3>Alda Protfolio</h3>
-        <div>
-          {moment().format("MMM Do YY")} 
-        </div>
+      <div className='container'>
+       
+        <Router>
+            <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+
+      
       </div>
     );
   }
